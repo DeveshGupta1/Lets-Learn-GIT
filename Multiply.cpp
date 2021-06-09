e0e500eaca83e2b9b5d1096af2a21cac2e7cacee
Devesh Gupta <devesh.gupta99@gmail.com>
2021-06-09 19:56:55 +0530

    adding Multiply.cpp

---
+++ Multiply.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"\nEnter 1st Number : ";
+    cin>>a;
+    cout<<"\nEnter 2nd Number : ";
+    cin>>b;
+    cout<<"\n Product is : "<<a*b;
+    return 0;
+}
