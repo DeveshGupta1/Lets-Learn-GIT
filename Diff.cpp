6efd5127bf3dbdaf43932f2e2bff86b1e4e0a21e
Devesh Gupta <devesh.gupta99@gmail.com>
2021-06-09 19:09:36 +0530

    adding Diff.cpp

---
+++ Diff.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"\nEnter 1st number : ";
+    cin>>a;
+    cout<<"\nEnter 2nd number : ";
+    cin>>b;
+    cout<<"\nDiffrence is : "<<a-b;
+    return 0;
+}
