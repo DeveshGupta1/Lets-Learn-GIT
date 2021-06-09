94c928fc5c4565802ed1696d1d653d9f7cf5bbf0
Devesh Gupta <devesh.gupta99@gmail.com>
2021-06-09 19:12:29 +0530

    Modified Sum.cpp

---
+++ Sum.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout<<"\nEnter 1st number : ";
+    cin>>a;
+    cout<<"\nEnter 2nd number : ";
+    cin>>b;
+    c=a+b;
+    cout<<"\nSum is : "<<c;
+    return 0;
+}
